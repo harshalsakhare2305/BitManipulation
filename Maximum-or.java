a2631446a89f4e5145d82d5ab111fe9487fa21b0
harshal Sakhare <118835450+harshalsakhare2305@users.noreply.github.com>
2025-01-20 16:11:04 +0530

    Create Maximum-or.java

---
+++ Maximum-or.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/maximum-or/
+class Solution {
+    public long maximumOr(int[] nums, int k) {
+        int n = nums.length;
+        long result = 0;
+        int[] prefix = new int[n + 1];
+        int[] suffix = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i] = prefix[i - 1] | nums[i - 1];
+            suffix[n - i - 1] = suffix[n - i] | nums[n - i];
+        }
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, prefix[i] | ((long)nums[i] << k) | suffix[i]);
+        }
+        return result;
+    }
+}
